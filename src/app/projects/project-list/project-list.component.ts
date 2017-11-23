e0a7ddbc1757f5af6deb10e88c4dd4f1a1b63507
vvik91 <veres.viktor6@gmail.com>
2017-11-23 14:45:23 +0100

    project list starring and public/private project

--- src/app/projects/project-list/project-list.component.ts
+++ src/app/projects/project-list/project-list.component.ts
@@ -16,12 +16,13 @@ import { LoginDialogService } from '../../login/login-dialog.service';
 })
 export class ProjectListComponent implements OnInit {
   @Input() projects: types.Project[] = [];
-  @Input() refresh: () => void;
+  @Input() refresh: () => any;
 
   search: string;
   currentPage = 0;
   author = '';
   isProjectsPage = false;
+  starred = false;
 
   constructor(
     private cp: CreateProjectDialogService,
@@ -63,11 +64,30 @@ export class ProjectListComponent implements OnInit {
     const that = this;
     this.http.put(this._const.url + '/v1/star', {
       'projectId': p.Id,
-      'token': this.ss.getToken(),
+      'token': that.ss.getToken(),
     }).subscribe(() => {
       p.Stars++;
+      p.Starrers = [];
     }, error => {
       console.log(error.error);
     });
+  }
+  unStar(proj: types.Project) {
+    const that = this;
+    let p = new HttpParams();
+    p = p.set('projectId', proj.Id);
+    p = p.set('token', this.ss.getToken());
+    this.http
+      .delete(this._const.url + '/v1/star', {
+        params: p
+      })
+      .subscribe(
+        () => {
+          proj.Stars--;
+          that.starred = false;
+          proj.Starrers = null;
+        },
+        error => {}
+      );
   }
 }
