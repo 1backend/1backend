39864484fc1a31468cc9e87eb3f1bfe28a84ba6f
Janos Dobronszki <dobronszki@gmail.com>
2017-12-06 14:00:09 +0100

    Password reset email sending fixes

--- backend/domain/email_dao.go
+++ backend/domain/email_dao.go
@@ -11,16 +11,15 @@ import (
 
 const pwResetEmail = `Hi %s,
 
-You can reset your password here: %ssiteUrl/reset/%s
+You can reset your password here: %s/reset/%s
 
-Cheers
+Cheers, 
 1Backend Team`
 
 func SendPasswordReset(secret string, user *User) error {
 	from := mail.NewEmail("1Backend", "passwordreset@1backend.com")
 	subject := "1Backend Password Reset"
 	to := mail.NewEmail(user.Name, user.Email)
-
 	name := user.Name
 	if name == "" {
 		name = user.Nick
@@ -32,12 +31,15 @@ func SendPasswordReset(secret string, user *User) error {
 	if config.C.SendGridKey == "" {
 		return errors.New("Email api key missing")
 	}
-	content := mail.NewContent("text/plain", fmt.Sprintf(pwResetEmail, config.C.SiteUrl, name, secret))
+	content := mail.NewContent("text/plain", fmt.Sprintf(pwResetEmail, name, config.C.SiteUrl, secret))
 	m := mail.NewV3MailInit(from, subject, to, content)
 
 	request := sendgrid.GetRequest(config.C.SendGridKey, "/v3/mail/send", "https://api.sendgrid.com")
 	request.Method = "POST"
 	request.Body = mail.GetRequestBody(m)
-	_, err := sendgrid.API(request)
+	rsp, err := sendgrid.API(request)
+	if rsp.StatusCode < 200 || rsp.StatusCode >= 300 {
+		return errors.New(fmt.Sprintf("%v", rsp))
+	}
 	return err
 }
