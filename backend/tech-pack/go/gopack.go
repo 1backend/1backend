c226558dae9b55e0e4ecc403e8c55e57e652b59d
Janos Dobronszki <dobronszki@gmail.com>
2017-12-09 13:36:57 +0100

    Default node, ts, go services all have type and input example endpoints now

--- backend/tech-pack/go/gopack.go
+++ backend/tech-pack/go/gopack.go
@@ -4,6 +4,7 @@ import (
 	"text/template"
 
 	"github.com/1backend/1backend/backend/domain"
+	"github.com/1backend/1backend/backend/tech-pack/utils"
 )
 
 func NewPack(project *domain.Project) GoPack {
@@ -21,8 +22,7 @@ func (g GoPack) RecipePath() string {
 }
 
 func (g GoPack) CreateProjectPlugin() error {
-	generateEndpoints(g.project)
-	return nil
+	return generateEndpoints(g.project)
 }
 
 func (g GoPack) Outfile() string {
@@ -41,11 +41,15 @@ const hi = `func (w http.ResponseWriter, r *http.Request, p httpr.Params) {
   utils.Write(w, "hi")
 }
 `
+const hiInput = `[]`
+const hiOutput = "string"
 
 const importedHi = `func (w http.ResponseWriter, r *http.Request, p httpr.Params) {
   example.Hi(w, r, p)
 }
 `
+const importedHiInput = `[]`
+const importedHiOutput = "string"
 
 const sqlExample = `func (w http.ResponseWriter, r *http.Request, p httpr.Params) {
 	rows, err := sqlClient.Query("SELECT 1 + 1 AS solution")
@@ -59,25 +63,59 @@ const sqlExample = `func (w http.ResponseWriter, r *http.Request, p httpr.Params
 	utils.Write(w, solution)
 }
 `
+const sqlExampleInput = `[]`
+const sqlExampleOutput = "string"
+
+const inputExample = `service.CalculateRectangleArea`
+const inputExampleInput = `[
+	{"rect": "rectangle"},
+	{"unit": "string"}
+]`
+const inputExampleOutput = `output`
+
+const types = `{
+	"rectangle": [
+		{"sideA": "number"},
+		{"sideB": "number"}
+	]
+}`
 
 const imports = `utils "github.com/1backend/go-utils"
 example "github.com/1backend/go-example-service"
 `
 
-func generateEndpoints(proj *domain.Project) {
+func generateEndpoints(proj *domain.Project) error {
+	proj.Description = "An empty Go project"
 	proj.Imports = imports
+	readme, err := utils.GetReadme(proj)
+	if err != nil {
+		return err
+	}
+	proj.ReadMe = readme
 	proj.Endpoints = []domain.Endpoint{
 		domain.Endpoint{
 			Url:         "/hi",
 			Method:      "GET",
 			Code:        hi,
-			Description: "A very simple endpoint in go, saying hi to you",
+			Input:       importedHiInput,
+			Output:      importedHiOutput,
+			Description: "A very simple endpoint in TypeScript, saying hi to you",
 		},
 		domain.Endpoint{
 			Url:         "/imported-hi",
 			Method:      "GET",
 			Code:        importedHi,
+			Input:       importedHiInput,
+			Output:      importedHiOutput,
 			Description: "An endpoint that demonstrates the usage of an imported package",
+		},
+		domain.Endpoint{
+			Url:         "/input-example",
+			Method:      "POST",
+			Code:        inputExample,
+			Input:       inputExampleInput,
+			Output:      inputExampleOutput,
+			Description: "An endpoint that demonstrates endoint input type usage",
 		},
 	}
 	for _, v := range proj.Dependencies {
@@ -92,4 +130,5 @@ func generateEndpoints(proj *domain.Project) {
 			}...)
 		}
 	}
+	return nil
 }
