073c26224b121194334a6bc46124c7fc7a8f50d9
Janos Dobronszki <dobronszki@gmail.com>
2017-12-13 21:46:38 +0100

    Increasing timeout numbers to have less false negative tests

--- e2e/pages/header.po.ts
+++ e2e/pages/header.po.ts
@@ -12,7 +12,7 @@ export class Header {
     return browser
       .wait(
         until.presenceOf(regSubmit),
-        5000,
+        9000,
         'Register tab taking too long to appear in the DOM'
       )
       .then(() => {
@@ -20,7 +20,7 @@ export class Header {
         loginPage.register(user);
         return browser.wait(
           until.titleContains(user.UserName),
-          5000,
+          9000,
           'Redirect to author page after register took too long'
         );
       });
@@ -42,7 +42,7 @@ export class Header {
       });
       return browser.wait(
         until.titleContains(user.UserName),
-        3000,
+        9000,
         'Redirect to author page after login took too long'
       );
     });
