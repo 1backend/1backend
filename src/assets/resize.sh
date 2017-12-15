a5d74eb6e25c8b17aca04becef8f9696552c651f
János Dobronszki <dobronszki@gmail.com>
2017-12-15 20:30:42 +0100

    Needs more cowbell and images (#76)

---
+++ src/assets/resize.sh
@@ -0,0 +1,6 @@
+convert zh.svg -resize 256x256 -transparent white favicon-256.png
+convert favicon-256.png -resize 16x16 favicon-16.png
+convert favicon-256.png -resize 32x32 favicon-32.png
+convert favicon-256.png -resize 64x64 favicon-64.png
+convert favicon-256.png -resize 128x128 favicon-128.png
+convert favicon-16.png favicon-32.png favicon-64.png favicon-128.png favicon-256.png -colors 256 favicon.ico
