445dd91fe8bf68868ad530dbb5d0491472d3ef35
vvik91 <veres.viktor6@gmail.com>
2018-01-19 18:16:56 +0100

    Delete project feature

--- src/app/projects/project/settings/settings.component.ts
+++ src/app/projects/project/settings/settings.component.ts
@@ -1,9 +1,17 @@
-import { Component, OnInit, ViewEncapsulation, Input, Output, EventEmitter } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  ViewEncapsulation,
+  Input,
+  Output,
+  EventEmitter
+} from '@angular/core';
 import * as types from '../../../types';
 import { ProjectService } from '../../../project.service';
 import { FormsModule } from '@angular/forms';
 import { UserService } from '../../../user.service';
 import { Router } from '@angular/router';
+import { NotificationsService } from 'angular2-notifications';
 
 @Component({
   selector: 'app-settings',
@@ -22,13 +30,25 @@ export class SettingsComponent implements OnInit {
   isPremiumMember = false;
   newProjectName;
   nameChanged = false;
+  deleteProjectName: string;
+  newVersion;
+  newNamespace;
+  newDescription;
 
-  constructor(private ps: ProjectService, private us: UserService, private router: Router) { }
+  constructor(
+    private ps: ProjectService,
+    private us: UserService,
+    private router: Router,
+    private notif: NotificationsService
+  ) {}
 
   ngOnInit() {
     this.isPremiumMember = this.us.user.Premium;
-    this.privateChecked != this.project.OpenSource;
+    this.privateChecked = !this.project.OpenSource;
     this.newProjectName = this.project.Name;
+    this.newVersion = this.project.Version;
+    this.newNamespace = this.project.Namespace;
+    this.newDescription = this.project.Description;
   }
 
   showCallerId() {
@@ -47,18 +67,44 @@ export class SettingsComponent implements OnInit {
   }
 
   saveSettings() {
-    if(this.project.Name != this.newProjectName) {
+    this.project.Version = this.newVersion;
+    this.project.Description = this.newDescription;
+    this.project.Namespace = this.newNamespace;
+    this.project.OpenSource = !this.privateChecked;
+    this.ps.update(this.project).then(() => {
+      this.onProjectSaved.emit();
+    });
+  }
+
+  deleteProject() {
+    if (this.project.Name !== this.deleteProjectName) {
+      this.notif.error('Incorrect project name');
+      return;
+    }
+    this.ps
+      .delete(this.project)
+      .then(() => {
+        this.router.navigate(['/' + this.us.user.Nick]);
+      })
+      .catch(err => {
+        this.notif.error(err);
+      });
+  }
+
+  changeName() {
+    if (this.project.Name !== this.newProjectName) {
       this.project.Name = this.newProjectName;
       this.nameChanged = true;
     }
-    this.project.OpenSource = !this.privateChecked;
     this.ps.update(this.project).then(() => {
-      if ( this.nameChanged ) {
-        this.router.navigate(['/' + this.project.Author + '/' + this.project.Name]);
+      if (this.nameChanged) {
+        this.router.navigate([
+          '/' + this.project.Author + '/' + this.project.Name
+        ]);
         this.onProjectSaved.emit();
       } else {
         this.onProjectSaved.emit();
       }
-    })
+    });
   }
 }
