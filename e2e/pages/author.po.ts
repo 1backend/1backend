424d121784a7879898d8967e18b57f57606ff2d6
Janos Dobronszki <dobronszki@gmail.com>
2018-02-13 14:26:08 +0000

    Increasing timeouts

--- e2e/pages/author.po.ts
+++ e2e/pages/author.po.ts
@@ -73,21 +73,21 @@ export class AuthorPage {
         cp.create(p);
         return browser.wait(
           until.invisibilityOf(utils.e('#project-status-red')),
-          30000,
+          40000,
           'Create project build took too long'
         );
       })
       .then(() => {
         return browser.wait(
           until.invisibilityOf(utils.e('#build-tab-icon-inprogress')),
-          30000,
+          40000,
           'Project build took too long'
         );
       })
       .then(() => {
         return browser.wait(
           until.presenceOf(utils.e('#build-tab-icon-success')),
-          30000,
+          40000,
           'Build is unsuccessful'
         );
       });
