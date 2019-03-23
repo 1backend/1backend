6826b92c7b05817af0d5b7c9d92c96d820ffb1e5
Janos Dobronszki <dobronszki@gmail.com>
2019-03-23 09:56:53 +0000

    Fix compile error dude to Stripe dependency change

--- backend/endpoints/charge.go
+++ backend/endpoints/charge.go
@@ -32,11 +32,12 @@ func (e Endpoints) Charge(accessToken, paymentToken string, amount uint64) error
 	// token is created using Checkout or Elements!
 	// token is the payment token ID submitted by the form
 
+	curr := "usd"
 	// Charge the user's card:
 	params := &stripe.ChargeParams{
-		Amount:   amount,
-		Currency: "usd",
-		Desc:     fmt.Sprintf("%v quota added", (amount/pricePer100k)*100000), // @todo hardcoded price
+		Amount:   &amount,
+		Currency: &curr,
+		//Desc:     fmt.Sprintf("%v quota added", (amount/pricePer100k)*100000), // @todo hardcoded price
 	}
 	err = params.SetSource(paymentToken)
 	if err != nil {
