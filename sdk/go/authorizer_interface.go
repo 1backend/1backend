c0ddb23ddde3700462106697e808783eeab43ca5
Janos Dobronszki <dobronszki@gmail.com>
2025-03-21 13:48:18 +0100

    feat(sdk): add helper functions to manage organization roles (#228)

--- sdk/go/authorizer_interface.go
+++ sdk/go/authorizer_interface.go
@@ -16,8 +16,24 @@ type Authorizer interface {
 	TokenFromRequest(r *http.Request) (string, bool)
 	ParseJWT(userSvcPublicKey, token string) (*Claims, error)
 	ParseJWTFromRequest(userSvcPublicKey string, r *http.Request) (*Claims, error)
+
+	// IsAdmin returns true if the user has
+	// the `user-svc:admin` role.
 	IsAdmin(userSvcPublicKey string, token string) (bool, error)
+
+	// IsAdminFromRequest returns true if the user has
+	// the `user-svc:admin` role.
 	IsAdminFromRequest(userSvcPublicKey string, r *http.Request) (bool, error)
+
+	// RolesInOrganization extracts organization-specific roles
+	// from a request. Given a role string like
+	// `user-svc:org:{org_dBZRCej3fo}:admin`, it returns `[admin]`.
+	RolesInOrganization(userSvcPublicKey string, token string) ([]string, error)
+
+	// RolesInOrganizationFromRequest extracts organization-specific roles
+	// from a request. Given a role string like
+	// `user-svc:org:{org_dBZRCej3fo}:admin`, it returns `[admin]`.
+	RolesInOrganizationFromRequest(userSvcPublicKey string, r *http.Request) ([]string, error)
 }
 
 type AuthorizerImpl struct{}
@@ -89,4 +105,37 @@ func (a AuthorizerImpl) IsAdmin(userSvcPublicKey, token string) (bool, error) {
 	}
 
 	return false, nil
+}
+
+func (a AuthorizerImpl) RolesInOrgFromRequest(userSvcPublicKey string, r *http.Request) ([]string, error) {
+	tokenString, hasToken := a.TokenFromRequest(r)
+	if !hasToken {
+		return nil, fmt.Errorf("no token found in request")
+	}
+
+	return a.RolesInOrg(userSvcPublicKey, tokenString)
+}
+
+func (a AuthorizerImpl) RolesInOrg(userSvcPublicKey, token string) ([]string, error) {
+	claims, err := a.ParseJWT(userSvcPublicKey, token)
+	if err != nil {
+		return nil, err
+	}
+
+	return ExtractOrganizationRoles(claims.RoleIds), nil
+}
+
+func ExtractOrganizationRoles(roleIds []string) []string {
+	ret := []string{}
+
+	for _, roleId := range roleIds {
+		// @todo constant
+		if strings.HasPrefix(roleId, "user-svc:org:{") {
+			v := strings.Split(roleId, "{")[1]
+			v = strings.Split(v, "}:")[1]
+			ret = append(ret, v)
+		}
+	}
+
+	return ret
 }
