48413aefde2c1e145ad3ec64db39380a6fcbd333
crufter <dobronszki@gmail.com>
2025-03-29 21:00:35 +0100

    fix test

--- server/internal/services/user/http_create_user_test.go
+++ server/internal/services/user/http_create_user_test.go
@@ -14,7 +14,7 @@ import (
 func TestCreateUser(t *testing.T) {
 	t.Parallel()
 
-	server, err := test.StartServer(test.Options{
+	server, err := test.StartService(test.Options{
 		Test: true,
 	})
 	require.NoError(t, err)
