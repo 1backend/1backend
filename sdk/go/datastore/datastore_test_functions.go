80e1c65c60410248db881225875e366cfeb48950
crufter <dobronszki@gmail.com>
2025-04-04 00:35:07 +0200

    feat(datastore): add indexing capability

--- sdk/go/datastore/datastore_test_functions.go
+++ sdk/go/datastore/datastore_test_functions.go
@@ -45,8 +45,109 @@ type TestObject struct {
 	NamedType         NamedString             `json:"namedType"`
 }
 
+func (t TestObject) Indexes() []Index {
+	return []Index{
+		{
+			Fields:    []string{"value"},
+			Ascending: false,
+		},
+	}
+}
+
 func (t TestObject) GetId() string {
 	return t.Name
+}
+
+// Doesn't really test indexes but helps us to play around,
+// printline some values and compare.
+func TestIndex(t *testing.T, store DataStore) {
+	store.SetDebug(false)
+
+	c := 0
+	for range 100 {
+		objs := []Row{}
+		for range 100 {
+			c++
+			objs = append(objs, TestObject{
+				Name:  fmt.Sprintf("%v", c),
+				Value: c,
+				Age:   c,
+			})
+		}
+
+		err := store.UpsertMany(objs)
+		require.NoError(t, err)
+	}
+
+	store.SetDebug(true)
+
+	now := time.Now()
+	records, err := store.Query().OrderBy(
+		OrderByField("value", true),
+	).After(5000).Limit(10).Find()
+	dur1 := time.Since(now)
+	require.NoError(t, err)
+	require.Len(t, records, 10)
+	require.Equal(t, "4999", records[0].GetId())
+	require.Equal(t, "4998", records[1].GetId())
+
+	now = time.Now()
+	records, err = store.Query().OrderBy(
+		OrderByField("age", true),
+	).After(5000).Limit(10).Find()
+	dur2 := time.Since(now)
+	require.NoError(t, err)
+	require.Len(t, records, 10)
+	require.Equal(t, "4999", records[0].GetId())
+	require.Equal(t, "4998", records[1].GetId())
+
+	fmt.Println("Duration with index", dur1, "without index", dur2)
+}
+
+// Doesn't really test indexes but helps us to play around,
+// printline some values and compare.
+func TestPointerIndex(t *testing.T, store DataStore) {
+	store.SetDebug(false)
+
+	c := 0
+	for range 100 {
+		objs := []Row{}
+		for range 100 {
+			c++
+			objs = append(objs, &TestObject{
+				Name:  fmt.Sprintf("%v", c),
+				Value: c,
+				Age:   c,
+			})
+		}
+
+		err := store.UpsertMany(objs)
+		require.NoError(t, err)
+	}
+
+	store.SetDebug(true)
+
+	now := time.Now()
+	records, err := store.Query().OrderBy(
+		OrderByField("value", true),
+	).After(5000).Limit(10).Find()
+	dur1 := time.Since(now)
+	require.NoError(t, err)
+	require.Len(t, records, 10)
+	require.Equal(t, "4999", records[0].GetId())
+	require.Equal(t, "4998", records[1].GetId())
+
+	now = time.Now()
+	records, err = store.Query().OrderBy(
+		OrderByField("age", true),
+	).After(5000).Limit(10).Find()
+	dur2 := time.Since(now)
+	require.NoError(t, err)
+	require.Len(t, records, 10)
+	require.Equal(t, "4999", records[0].GetId())
+	require.Equal(t, "4998", records[1].GetId())
+
+	fmt.Println("Duration with index", dur1, "without index", dur2)
 }
 
 func TestRandomize(t *testing.T, store DataStore) {
