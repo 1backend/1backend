16f9736cce2824c285ac7affc31a91f4279a7a18
Janos Dobronszki <dobronszki@gmail.com>
2025-04-05 14:48:04 +0200

    fix: ui after major changes to roles and permissions (#282)

--- desktop/workspaces/angular-app/src/app/users/add-user/add-user.component.ts
+++ desktop/workspaces/angular-app/src/app/users/add-user/add-user.component.ts
@@ -20,17 +20,14 @@ import {
 	ToastController,
 	IonCardContent,
 	IonItem,
-	IonSelectOption,
-	IonSelect,
 	IonRow,
 	IonCol,
 	IonButton,
 } from '@ionic/angular/standalone';
 import { TranslatePipe } from '../../translate.pipe';
 import { TranslateModule } from '@ngx-translate/core';
-import { NgFor } from '@angular/common';
 import { CenteredComponent } from '../../components/centered/centered.component';
-import { ChangeDetectorRef, ChangeDetectionStrategy } from '@angular/core';
+import { ChangeDetectionStrategy } from '@angular/core';
 import { PageComponent } from '../../components/page/page.component';
 import { IconMenuComponent } from '../../components/icon-menu/icon-menu.component';
 
@@ -44,14 +41,11 @@ import { IconMenuComponent } from '../../components/icon-menu/icon-menu.componen
 		IonButton,
 		IonCardContent,
 		IonItem,
-		IonSelectOption,
-		IonSelect,
 		PageComponent,
 		IconMenuComponent,
 		CenteredComponent,
 		FormsModule,
 		ReactiveFormsModule,
-		NgFor,
 		TranslateModule,
 		TranslatePipe,
 	],
@@ -59,17 +53,13 @@ import { IconMenuComponent } from '../../components/icon-menu/icon-menu.componen
 })
 export class AddUserComponent implements OnInit {
 	addUserForm!: FormGroup;
-	roles:string[] = [];
 
 	constructor(
 		private fb: FormBuilder,
 		private userService: UserService,
-		private toast: ToastController,
-		private cd: ChangeDetectorRef
+		private toast: ToastController
 	) {
-		this.userService.user$.pipe(first()).subscribe(() => {
-			this.loggedInInit();
-		});
+		this.userService.user$.pipe(first()).subscribe(() => {});
 	}
 
 	ngOnInit() {
@@ -80,12 +70,6 @@ export class AddUserComponent implements OnInit {
 			passwordConfirmation: ['', Validators.required],
 			roles: [[], Validators.required],
 		});
-	}
-
-	async loggedInInit() {
-		const rsp = await this.userService.getRoles();
-		this.roles = rsp.roles!;
-		this.cd.markForCheck();
 	}
 
 	async createUser() {
