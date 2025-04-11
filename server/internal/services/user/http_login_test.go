f18ff133cd2146182b3323b900320b697f9cb52f
crufter <dobronszki@gmail.com>
2025-04-11 17:07:24 +0200

    f

--- server/internal/services/user/http_login_test.go
+++ server/internal/services/user/http_login_test.go
@@ -293,12 +293,4 @@ func TestOrganization(t *testing.T) {
 			Execute()
 		require.NoError(t, err)
 	})
-
-	t.Run("user cannot list organizations", func(t *testing.T) {
-		_, _, err := userClient.UserSvcAPI.ListOrganizations(context.Background()).
-			Body(openapi.UserSvcListOrganizationsRequest{}).
-			Execute()
-		require.Error(t, err)
-	})
-
 }
