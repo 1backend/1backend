5a550f83d42044794a99a0013dfc421119a3282a
crufter <dobronszki@gmail.com>
2025-05-13 11:52:25 +0200

    chore: version bump to 0.5.0

--- clients/go/client.go
+++ clients/go/client.go
@@ -3,7 +3,7 @@
 
 AI-native microservices platform.
 
-API version: 0.4.3
+API version: 0.5.0
 Contact: sales@singulatron.com
 */
 
@@ -42,7 +42,7 @@ var (
 	queryDescape    = strings.NewReplacer( "%5B", "[", "%5D", "]" )
 )
 
-// APIClient manages communication with the 1Backend API v0.4.3
+// APIClient manages communication with the 1Backend API v0.5.0
 // In most cases there should be only one, shared, APIClient.
 type APIClient struct {
 	cfg    *Configuration
