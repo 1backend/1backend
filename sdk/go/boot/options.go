ec637c322270f1b0802c69e96f19b099d760b431
crufter <dobronszki@gmail.com>
2025-05-21 21:39:30 +0200

    checkpoint

--- sdk/go/boot/options.go
+++ sdk/go/boot/options.go
@@ -12,15 +12,30 @@
 */
 package boot
 
-import "os"
+import (
+	"net/http"
+	"os"
+
+	"github.com/1backend/1backend/sdk/go/auth"
+	"github.com/1backend/1backend/sdk/go/client"
+	"github.com/1backend/1backend/sdk/go/endpoint"
+	"github.com/1backend/1backend/sdk/go/middlewares"
+	"github.com/pkg/errors"
+)
 
 type Options struct {
 	Test      bool
 	ServerUrl string
 	SelfUrl   string
+
+	TokenAutoRefreshOff bool
+	ClientFactory       client.ClientFactory
+	TokenRefresher      endpoint.TokenRefresher
+	Middlewares         func(http.HandlerFunc) http.HandlerFunc
+	Authorizer          auth.Authorizer
 }
 
-func (o *Options) LoadEnvars() {
+func (o *Options) LoadEnvars() error {
 	if o.ServerUrl == "" {
 		o.ServerUrl = os.Getenv("OB_SERVER_URL")
 	}
@@ -36,4 +51,37 @@ func (o *Options) LoadEnvars() {
 	if !o.Test && os.Getenv("OB_TEST") == "true" {
 		o.Test = true
 	}
+
+	if o.ClientFactory == nil {
+		o.ClientFactory = client.NewApiClientFactory(o.SelfUrl)
+	}
+
+	if o.Authorizer == nil {
+		o.Authorizer = auth.AuthorizerImpl{}
+	}
+
+	if o.TokenRefresher == nil {
+		var err error
+		o.TokenRefresher, err = endpoint.NewTokenRefresher(
+			o.ClientFactory,
+			o.Authorizer,
+		)
+		if err != nil {
+			return errors.Wrap(err, "failed to create token refresher")
+		}
+	}
+
+	if os.Getenv("OB_TOKEN_AUTO_REFRESH_OFF") == "true" {
+		o.TokenAutoRefreshOff = true
+	}
+
+	if o.Middlewares == nil {
+		mws := middlewares.Applicator(
+			o.TokenRefresher,
+			o.TokenAutoRefreshOff,
+		)
+		o.Middlewares = mws
+	}
+
+	return nil
 }
