fc8980d6883c3d9b3f89d07b96ffff714b9fae88
Janos Dobronszki <dobronszki@gmail.com>
2025-06-06 19:36:51 +0200

    feat: redirect stdlib log to slog with severity heuristics (#387)

--- sdk/go/logger/logger.go
+++ sdk/go/logger/logger.go
@@ -10,6 +10,7 @@ package logger
 import (
 	"log"
 	"log/slog"
+	"strings"
 )
 
 var jsonHandler = slog.NewJSONHandler(log.Writer(), &slog.HandlerOptions{
@@ -22,3 +23,32 @@ var Debug = Logger.Debug
 var Info = Logger.Info
 var Warn = Logger.Warn
 var Error = Logger.Error
+
+func init() {
+	//
+	// Some libraries used like autocert does not use slog directly,
+	// so we need to redirect the stdlib log to our custom slog logger.
+	//
+
+	// Redirect stdlib log to your custom writer
+	log.SetOutput(&logProxyWriter{})
+}
+
+// logProxyWriter inspects the message and routes to slog with a guessed level
+type logProxyWriter struct{}
+
+func (w *logProxyWriter) Write(p []byte) (n int, err error) {
+	msg := strings.TrimSpace(string(p))
+
+	lower := strings.ToLower(msg)
+
+	// Heuristics: look for keywords
+	switch {
+	case strings.Contains(lower, "error"), strings.Contains(lower, "fail"):
+		Logger.Error("stdlog", "msg", msg)
+	default:
+		Logger.Info("stdlog", "msg", msg)
+	}
+
+	return len(p), nil
+}
