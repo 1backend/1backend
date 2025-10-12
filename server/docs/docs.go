a228ae3db0c344fe46844c1d3c10eea4fd8aac2b
Janos Dobronszki <dobronszki@gmail.com>
2025-10-12 08:36:55 +0200

    bump and regen

--- server/docs/docs.go
+++ server/docs/docs.go
@@ -9,7 +9,7 @@ const docTemplate = `{
 
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
-	Version:          "0.9.0",
+	Version:          "0.9.1",
 	Host:             "localhost:11337",
 	BasePath:         "/",
 	Schemes:          []string{},
--- server/main.go
+++ server/main.go
@@ -22,7 +22,7 @@ import (
 )
 
 // @title           1Backend
-// @version         0.9.0
+// @version         0.9.1
 // @description     AI-native microservices platform.
 // @termsOfService  http://swagger.io/terms/
 
